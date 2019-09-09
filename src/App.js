f66ed81e353f6a25f86ac699d82b23bc8df0855e
Matthew Yee <matthewyee@Matthews-MacBook-Pro.local>
2019-09-09 15:44:02 -0700

    async await added to req axios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+  // componentDidMount() {
+  //   axios
+  //     .get("https://api.github.com/users")
+  //     .then(res => console.log(res.data));
+  // }
+  // async componentDidMount() {
+  //   this.setState({ loading: true })
+
+  //   const res = await axios.get("https://api.github.com/users")
+
+  //   this.setState({ users: res.data, loading: false })
+  // }
+
+  // Search Github users
+  searchUsers = async (text) => {
+    this.setState({ loading: true })
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}`)
+
+    this.setState({ users: res.data.items, loading: false })
+  }
+
+  render() {
+
+    const { loading, users } = this.state;
+
+    return (
+      <div>
+        <Navbar title="Destiny Carry" icon="fab fa-github" />
+        <Search searchUsers={this.searchUsers}/>
+        <Users loading={loading} users={users}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/users/Search.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class Search extends Component {
+  state = {
+    text: ""
+  };
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    this.props.searchUsers(this.state.text)
+    this.setState({ text: '' })
+  }
+
+  render() {
+    const { text } = this.state;
+    const { onChange, onSubmit } = this;
+    return (
+      <div>
+        <form onSubmit={onSubmit} className="form">
+          <input
+            type="text"
+            name="text"
+            placeholder="Search Users..."
+            value={text}
+            onChange={onChange}
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
